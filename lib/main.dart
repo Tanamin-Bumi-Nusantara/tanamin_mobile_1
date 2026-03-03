7e7a75a4f1447e8c38180fc12239628c0e0eaffc
Faiq Athari <faiq.id2017@gmail.com>
2026-03-03 16:05:49 +0800

    chore : add dummy API (json) & and update pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tanamin_mobile_1/features/campaign/presentation/pages/history_page.dart';
-import 'package:tanamin_mobile_1/features/campaign/presentation/pages/monitoring_page.dart';
-import 'package:tanamin_mobile_1/features/home/presentation/pages/notifications_page.dart';
-import 'package:tanamin_mobile_1/features/home/presentation/pages/settings_page.dart';
 import 'package:tanamin_mobile_1/features/profile/presentation/pages/edit_email_page.dart';
-import 'package:tanamin_mobile_1/features/profile/presentation/pages/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: const EditEmailPage());
